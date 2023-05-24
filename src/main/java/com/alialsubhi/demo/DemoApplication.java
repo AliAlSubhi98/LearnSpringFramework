35b4d2b9d7622bbc043bc34f8f7d32ab80e0913a
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-24 23:11:01 +0400

    Tight Coupling vs Loose Coupling <comment>

--- src/main/java/com/alialsubhi/demo/DemoApplication.java
+++ src/main/java/com/alialsubhi/demo/DemoApplication.java
@@ -11,7 +11,15 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+/*
+### Tight Coupling vs Loose Coupling
+* **Tight Coupling:** When two classes are tightly coupled, they are dependent on each other. If one class changes, the other class will also change.
 
+* **Loose Coupling:** When two classes are loosely coupled, they are not dependent on each other. If one class changes, the other class will not change.
+
+In the example below, GameRunner class has a dependency on GamingConsole. Instead of wiring game object to a specific class such as MarioGame, we can use GamingConsole interface to make it loosely coupled. So that, we don't need to change our original code. In the future, we can create classes that implements GamingConsole interface (Polymorphism) and use it.
+
+ */
 		//var marioGame =  new MarioGame();
 		var superContraGame = new SuperContraGame();
 		var gameRunner = new GameRunner(superContraGame);
