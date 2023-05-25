73ea913cc16ed79c1c260483a1d98ae3db6a5160
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-26 00:44:20 +0400

    Creating More Java Spring Beans in Spring Configuration File

--- src/main/java/com/alialsubhi/demo/HelloWorldConfiguration.java
+++ src/main/java/com/alialsubhi/demo/HelloWorldConfiguration.java
@@ -2,11 +2,31 @@ package com.alialsubhi.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+record Person (String name , int age){};
 
+//Address - firstLine + city
+record Address (String firstLine, String city){};
 @Configuration
 public class HelloWorldConfiguration {
     @Bean
     public String name(){
         return "ALi AlSubhi Trying";
+    }
+
+    @Bean
+    public int age(){
+        return 25;
+    }
+
+    @Bean
+    public Person person(){
+        var person = new Person("Ahmed AlSubhi",23);
+        return person;
+    }
+
+    @Bean
+    public Address address(){
+        var address = new Address("Al Maabilah","Seeb");
+        return address;
     }
 }
