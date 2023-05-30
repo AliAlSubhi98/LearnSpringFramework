097b20b330da76d288a911b824bd582f298f5992
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-05-30 20:07:52 +0400

    Setter Injection

--- src/main/java/com/alialsubhi/learnspringframwork/examples/a1/DepInjectionLauncherApplication.java
+++ src/main/java/com/alialsubhi/learnspringframwork/examples/a1/DepInjectionLauncherApplication.java
@@ -12,14 +12,21 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 @Component
 class MyBusinessClass{
-
-	// Field Injection no setters or constructor
-	@Autowired
+	
 	Dependency1 dependency1;
-	@Autowired
 	Dependency2 dependency2;
 
+	@Autowired
+	public void setDependency1(Dependency1 dependency1) {
+		System.out.println("Setter Injection = setDependency1");
+		this.dependency1 = dependency1;
+	}
+	@Autowired
+	public void setDependency2(Dependency2 dependency2) {
+		System.out.println("Setter Injection = setDependency2");
 
+		this.dependency2 = dependency2;
+	}
 
 	public String toString(){
 		return "using " + dependency1 + " and " + dependency2;
